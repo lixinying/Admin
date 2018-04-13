0445330eded2d55a17a164724a185d0d81685fde
lixinying <lixinyingok@gmail.com>
2018-04-13 16:48:58 +0800

    base version 1

--- FrontEnd/app/router.js
+++ FrontEnd/app/router.js
@@ -1,17 +1,12 @@
  app.config(
       function ($stateProvider,   $urlRouterProvider) {
           $urlRouterProvider
-              .otherwise('/login');//默认路由也是登录的第一个
+              .otherwise('login');//默认路由也是登录的第一个
           $stateProvider
               .state('login',{
                   url:'/login',
                   templateUrl:'app/Auth/login.html',
-                   resolve: {
-                      deps: ['$ocLazyLoad',
-                        function( $ocLazyLoad ){
-                          return $ocLazyLoad.load('app/Auth/ctrl.js');
-                      }]
-                  }
+                  
               })
       
               .state('app', {
