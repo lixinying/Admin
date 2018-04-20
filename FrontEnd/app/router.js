11b746eb378ec20d3cdfc666438ef2b6a8bd2703
lixinying <lixinyingok@gmail.com>
2018-04-20 17:56:48 +0800

    what changed

--- FrontEnd/app/router.js
+++ FrontEnd/app/router.js
@@ -1,58 +1,58 @@
- app.config(
-     function($stateProvider, $urlRouterProvider) {
-         $urlRouterProvider
-             .otherwise('login'); //默认路由也是登录的第一个
-         $stateProvider
-             .state('login', {
-                 url: '/login',
-                 templateUrl: 'app/Auth/login.html',
-                 controller: 'LoginController',
-                 resolve: {
-                     loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
-                         return $ocLazyLoad.load('app/auth/ctrl.js');
-                     }]
-                 }
+app.config(
+    function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise('login'); //默认路由也是登录的第一个
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'app/Auth/login.html',
+                controller: 'LoginController',
+                resolve: {
+                    loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+                        return $ocLazyLoad.load('app/auth/ctrl.js');
+                    }]
+                }
 
-             })
+            })
 
-             .state('app', {
-                 abstract: true,
-                 url: '/app',
-                 templateUrl: 'app/app.html',
+            .state('app', {
+                abstract: true,
+                url: '/app',
+                templateUrl: 'app/app.html',
 
-             })
-             .state('app.product', {
-                 url: '/product',
-                 templateUrl: 'app/product/product.html',
-                    controller: 'ProductController',
-                  resolve: {
-                     loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+            })
+            .state('app.product', {
+                url: '/product',
+                templateUrl: 'app/product/product.html',
+                controller: 'ProductController',
+                resolve: {
+                    loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
 
 
-                         return $ocLazyLoad.load('app/Product/ctrl.js');
-                     }]
-                 }
-             })
-             .state('app.dashboard', {
-                 url: '/dashboard',
-                 templateUrl: 'app/Dashboard/dashboard.html',
-                 controller: 'DashboardController',
-                 resolve: {
-                     loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
-                         return $ocLazyLoad.load('app/Dashboard/ctrl.js');
-                     }]
-                 }
-             })
-     }
- )
- app.run(
-     function($rootScope, $state, $stateParams, $localStorage, $http) {
-         $http.defaults.headers.common['Authorization'] = 'Basic ' + $localStorage.auth;
-         $rootScope.$state = $state;
-         $rootScope.$stateParams = $stateParams;
-         $rootScope.$on('$stateChangeSuccess', function(event, to, toParams, from, fromParams) {
-             $rootScope.previousState = from;
-             $rootScope.previousStateParams = fromParams;
-         });
-     }
- )
+                        return $ocLazyLoad.load('app/Product/ctrl.js');
+                    }]
+                }
+            })
+            .state('app.dashboard', {
+                url: '/dashboard',
+                templateUrl: 'app/Dashboard/dashboard.html',
+                controller: 'DashboardController',
+                resolve: {
+                    loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+                        return $ocLazyLoad.load('app/Dashboard/ctrl.js');
+                    }]
+                }
+            })
+    }
+)
+app.run(
+    function($rootScope, $state, $stateParams, $localStorage, $http) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + $localStorage.auth;
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        $rootScope.$on('$stateChangeSuccess', function(event, to, toParams, from, fromParams) {
+            $rootScope.previousState = from;
+            $rootScope.previousStateParams = fromParams;
+        });
+    }
+)
